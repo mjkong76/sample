b92843f7d00ee54d86eab1e3cc7a60ed25da2846
MJ.Kong <mjkong76@gmail.com>
2018-04-07 23:04:37 +0900

    Add Restaurant.swift

---
+++ FoodPin/FoodPin/Restaurant.swift
@@ -0,0 +1,13 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by 공명진 on 2018. 4. 7..
+//  Copyright © 2018년 mjkong. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    let names: [String] = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+}
